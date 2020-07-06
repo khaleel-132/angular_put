3d292ba20f1e73f53c52d5e0859ab24526e32417
khaleel-132 <khaleel.alungal132@gmail.com>
2020-07-06 22:17:57 +0530

    put

--- api/data.php
+++ api/data.php
@@ -1,8 +1,20 @@
 <?php
 require '../dbconfig.php';
-$post = file_get_contents('php://input');
+$post = file_get_contents("php://input");
 $content = (array)json_decode($post,true);
 
-$sql = 'INSERT INTO api (`name`) VALUES ('".$content[0]['name']."')';
+$sql ="insert into api ('name') values('" . $content[0]['name'] . "')";
 $result = $mysqli->query($sql);
+$lastid= $mysqli->insert_id;
+
+$query = "select * from api where id = '".$lastid."' ";
+$res = $mysqli->query($query);
+$arr1 = array();
+while ($row = $res->fetch_assoc())
+{
+    $arr1[] = $row;
+
+}
+
+echo json_encode($arr1);
 ?>
